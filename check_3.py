e3ff28bb69fdfc82b97213f1209990e4447292e8
vlirsub <vlirsub@gmail.com>
2018-10-04 17:02:58 +0300

    -

--- check_3.py
+++ check_3.py
@@ -30,40 +30,45 @@ print('Dataset memory usage {:.3} MB'.format(df.memory_usage().sum() / 1024 / 10
 df_test = pd.read_csv(test_csv)
 print('Test Dataset read, shape {}'.format(df.shape))
 y_true = pd.read_csv(target_csv)
-y_true = y_true[df_test['id_0'] == 101]
 
 df_y = df.target
 df_X = df.drop('target', axis=1)
 df_X = df.copy()
 
-df_X = transform_datetime_features(df_X)
-df_test = transform_datetime_features(df_test)
+#df_X = transform_datetime_features(df_X)
+#df_test = transform_datetime_features(df_test)
 
-df_X.head()
-df_X.describe()
+#df_X.head()
+#df_X.describe()
 
-df_X['id_0'].value_counts()
+number_columns = [
+    col_name
+    for col_name in df_X.columns
+    if col_name.startswith('number')
+]
 
-df_X['id_0'].unique()
-df_u = df_X[df_X['id_0'] == 102]
-#df_u[['datetime_0', 'target']].plot()
-df_y = df_X[df_X['id_0'] == 102].target
+def f_trans(x):
+    for cn in number_columns:
+        for i in range(1, 2 * 7 + 1):
+            x['{}_s{}'.format(cn, i)] = x[cn].shift(i)
+    return x
 
-df_u_test = df_test[df_test['id_0'] == 102]
+df_X_s = df_X[['id_0'] + number_columns].groupby('id_0').apply(f_trans)
+df_test_s = df_test[['id_0'] + number_columns].groupby('id_0').apply(f_trans)
 
-plt.plot(df_u['datetime_0'], df_u['target'])
-plt.plot(df_u_test['datetime_0'], y_true['target'])
-
+# Проверяем
+#df_X_s[df_X_s['id_0'] == 500]
+#df_test_s[df_test_s['id_0'] == 500]
 
 # Удаление колонок с постоянными значениями
 constant_columns = [
     col_name
-    for col_name in df_X.columns
-    if df_X[col_name].nunique() == 1
+    for col_name in df_X_s.columns
+    if df_X_s[col_name].nunique() == 1
 ]
-df_X.drop(constant_columns, axis=1, inplace=True)
+df_X_s.drop(constant_columns, axis=1, inplace=True)
 
-ONEHOT_MAX_UNIQUE_VALUES = 20
+ONEHOT_MAX_UNIQUE_VALUES = 32
 def prepare(df):
     categorical_values = {}
     for col_name in list(df.columns):
@@ -75,48 +80,34 @@ def prepare(df):
 
     return df
 
-df_X = prepare(df_X)
-df_test = prepare(df_test)
+#df_X = prepare(df_X)
+#df_test = prepare(df_test)
 
 # Корреляция
 correlations = np.abs([
-    np.corrcoef(df_y, df_X[col_name])[0, 1]
-    for col_name in df_X.columns if col_name.startswith('number')
+    np.corrcoef(df_y, df_X_s[col_name].fillna(-1).values)[0, 1]
+    for col_name in df_X_s.columns if col_name.startswith('number')
 ])
-np.sort(correlations)
+np.sort(correlations)[-10:]
 
 # Посмотрим на данные
-[ df_X[col].unique().shape[0] for col in df_X.columns]
+#[ df_X[col].unique().shape[0] for col in df_X.columns]
 
-
-df_X.iloc[:, 15].unique().shape[0]
-df_X.iloc[:, 15].value_counts().sort_values()
-df_X.iloc[:, 41].value_counts().sort_values()
-# 6, 10, 11, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40 - категория
-
-# Посмотрим на зависимость целевой переменной от остальных
-#
-
-plt.scatter(df_u.iloc[:, 33], df_u['target'])
-plt.xlabel('x')
-plt.ylabel('target')
-
-plt.plot(x=df_X.iloc[:, 6].values, y=df_y.values)
-
+# Используемые для обучения колонки
 used_columns = [
     col_name
-    for col_name in df_X.columns
-    if col_name.startswith('number') or col_name.startswith('onehot')
+    for col_name in df_X_s.columns
+    if col_name.startswith('number') or col_name.startswith('onehot') or col_name.startswith('dt')
 ]
-X_values = df_u[used_columns].fillna(-1).values
-X_values = df_X.values
+X_values = df_X_s[used_columns].fillna(-1).values
+#X_values = df_X.values
 
-X_test = prepare(df_test[used_columns].copy()).values
-X_test = prepare(df_test.copy()).values
-X_test = df_u_test[used_columns].fillna(-1).values
+#X_test = prepare(df_test[used_columns].copy()).values
+#X_test = prepare(df_test.copy()).values
+X_test = df_test_s[used_columns].fillna(-1).values
 
-model = LGBMRegressor(n_estimators=100)
-model = Ridge()
+model = LGBMRegressor(n_estimators=200)
+#model = Ridge(normalize=True)
 model.fit(X_values, df_y)
 prediction = model.predict(X_test)
 result = y_true.copy()
@@ -125,13 +116,15 @@ result['prediction'] = prediction
 metric = mean_squared_error(result['target'], result['prediction'])
 print('RMSE: {}'.format(metric))
 # Отправлено
-#1210754018
-#1204794882
-
-result['prediction'].plot()
+#1748460185
+#11925728503 Отправлено
+#12067769616
+#11555808147
+#11405518348
+#11385751169 200
 
 #%% Важность признаков
-fi = pd.DataFrame(list(zip(df_X.columns[2:], model.feature_importances_)), columns=('clm', 'imp'))
+fi = pd.DataFrame(list(zip(used_columns, model.feature_importances_)), columns=('clm', 'imp'))
 fi.sort_values(by='imp', inplace=True, ascending=False)
 
 # eof
